3f5f484232ea95ec1f7b7a8a27cd296443595e0c
Md. Rakibuz Sultan Pranto <pranto055@gmail.com>
2019-02-16 01:22:10 +0600

    Add files via upload

---
+++ Tree/BST_delete.c
@@ -0,0 +1,122 @@
+#include<stdio.h>
+typedef struct Node Node;
+struct Node{
+  int data;
+   Node* left;
+   Node* right;
+
+};
+
+
+//Function to find minimum in a tree.
+Node* FindMin(Node* root)
+{
+	while(root->left != NULL) root = root->left;
+	return root;
+}
+
+
+// Function to search a delete a value from tree.
+struct Node* Delete(struct Node *root, int data) {
+	if(root == NULL) return root;
+	else if(data < root->data) root->left = Delete(root->left,data);
+	else if (data > root->data) root->right = Delete(root->right,data);
+	// Wohoo... I found you, Get ready to be deleted
+	else {
+		// Case 1:  No child
+		if(root->left == NULL && root->right == NULL) {
+			free(root);
+			root = NULL;
+		}
+		//Case 2: One child
+		else if(root->left == NULL) {
+			struct Node *temp = root;
+			root = root->right;
+			free(temp);
+		}
+		else if(root->right == NULL) {
+			struct Node *temp = root;
+			root = root->left;
+			free (temp);
+		}
+		// case 3: 2 children
+		else {
+			struct Node *temp = FindMin(root->right);
+			root->data = temp->data;
+			root->right = Delete(root->right,temp->data);
+		}
+	}
+	return root;
+}
+
+Node *GetNewNode(int x){
+
+ Node *newNode=(Node*)malloc(sizeof(Node));
+if(newNode==NULL){
+    // if it could not allocate memory
+    printf("Error! Could not Create a New Node\n");
+    exit(1);
+}
+   newNode->data = x;
+   newNode->left = NULL;
+   newNode->right = NULL;
+   return newNode;
+}
+
+
+// To insert data in BST, returns address of root node
+Node* Insert(Node* root,int data) {
+	if(root == NULL) { // empty tree
+		root = GetNewNode(data);
+	}
+	// if data to be inserted is lesser, insert in left subtree.
+	else if(data <= root->data) {
+		root->left = Insert(root->left,data);
+	}
+	// else, insert in right subtree.
+	else {
+		root->right = Insert(root->right,data);
+	}
+	return root;
+}
+
+void in_order(Node *node){
+
+
+
+if(node->left !=NULL){
+    in_order(node->left);
+}
+printf("%d ",node->data);
+
+if(node->right !=NULL){
+    in_order(node->right);
+}
+
+
+}
+
+
+int main(){
+
+	/*Code To Test the logic
+	  Creating an example tree
+                5
+			   / \
+			  3   10
+			 / \   \
+			1   4   11
+    */
+	Node* root = NULL;
+	root = Insert(root,5); root = Insert(root,10);
+	root = Insert(root,3); root = Insert(root,4);
+	root = Insert(root,1); root = Insert(root,11);
+
+
+// Deleting node with value 5, change this value to test other cases
+   root = Delete(root,5);
+
+   in_order(root);
+
+}
+
---
+++ Tree/inorder_binarytree.c
@@ -0,0 +1,96 @@
+#include<stdio.h>
+typedef struct Node Node;
+struct Node{
+  int data;
+   Node* left;
+   Node* right;
+
+};
+
+Node *GetNewNode(int x){
+
+ Node *newNode=(Node*)malloc(sizeof(Node));
+if(newNode==NULL){
+    // if it could not allocate memory
+    printf("Error! Could not Create a New Node\n");
+    exit(1);
+}
+   newNode->data = x;
+   newNode->left = NULL;
+   newNode->right = NULL;
+   return newNode;
+}
+
+void add_left_child(Node *node, Node *child)
+{
+
+    node->left=child;
+}
+
+void add_right_child(Node *node, Node*child)
+{
+    node->right=child;
+}
+
+Node *create_tree(){
+
+
+  Node *two=GetNewNode(2);
+  Node *seven=GetNewNode(7);
+  Node *nine=GetNewNode(9);
+  add_left_child(two,seven);
+  add_right_child(two,nine);
+
+  Node *one=GetNewNode(1);
+  Node *six=GetNewNode(6);
+  add_left_child(seven,one);
+  add_right_child(seven,six);
+
+  Node *eight=GetNewNode(8);
+  add_right_child(nine,eight);
+
+  Node *five=GetNewNode(5);
+  Node *ten=GetNewNode(10);
+  add_left_child(six,five);
+  add_right_child(six,ten);
+
+  Node *three=GetNewNode(3);
+  Node *four=GetNewNode(4);
+  add_left_child(eight,three);
+  add_right_child(eight,four);
+
+  return two;
+
+}
+
+
+void in_order(Node *node){
+
+
+
+if(node->left !=NULL){
+    in_order(node->left);
+}
+printf("%d ",node->data);
+
+if(node->right !=NULL){
+    in_order(node->right);
+}
+
+
+}
+
+int main(){
+
+ Node *root=create_tree();
+ in_order(root);
+ printf("\n");
+
+return 0;
+
+
+
+}
+
+
+
